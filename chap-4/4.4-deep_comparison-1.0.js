9a80e726be159e8d00a8865eefc29f8e4eb1d08d
Arshad Khan <arshdkhn1@gmail.com>
2016-05-01 09:51:14 +0530

    adding final deep_comparison exercise and deep_comparision-1.0

--- chap-4/4.4-deep_comparison-1.0.js
+++ chap-4/4.4-deep_comparison-1.0.js
@@ -1,20 +1,17 @@
 function deepEqual(obj1, obj2){
-  if(obj1 === obj2)
-    return true;
-  if((obj1 == null || obj2 == null) || (typeof obj1 != "object" || typeof obj2 != "object"))
-  // this exp is necessary to filter all the types of non-object variables (including null)
-  // eg of non-object variables are number , string , undefined , NaN (number), boolean
-    return false;
-  var length1 = 0, length2 = 0;
-  for(var prop in obj1)
-    length1++;
-  for(var prop in obj2){
-    length2++;
-    if(!(prop in obj1) || !deepEqual(obj1[prop], obj2[prop]))// used bracket notation because property name may be non-conventional.
-      return false;
+  if(typeof obj1 == "object" && typeof obj2 == "object" && obj1 != null && obj2 != null){
+    var length1 = 0, length2 = 0;
+    for(var prop in obj1)
+      length1++;
+    for(var prop in obj2)
+      if(!prop in obj1 || !deepEqual(obj1[prop], obj2[prop]))
+        return false;
   }
 
-  return length1 === length2;
+  else if(obj1 !== obj2)
+    return false;
+
+  return true;// if false is not returned surely the two arguments will be deep equal.
 }
 
 var obj1 = {
@@ -76,4 +73,3 @@ console.log("obj1 is equal to obj3 :", deepEqual(obj1,obj3)); //false
 console.log("obj1 is equal to obj4 :", deepEqual(obj1,obj4)); //false
 console.log("obj1 is equal to obj5 :", deepEqual(obj1,obj5)); //true
 console.log("obj1 is equal to null :", deepEqual(obj1,null)); //false
-console.log("undefined is equal to undefined :", deepEqual(undefined,undefined)); //true
